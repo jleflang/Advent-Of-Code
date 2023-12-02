6dbe3d70b564d32308b65a7c419b833a155e4679
James Leflang <59455417+jleflang@users.noreply.github.com>
2023-12-02 10:43:38 -0800

    Day 2 Solution
    Valid A and B solutions!

--- 2023/src/problem.rs
+++ 2023/src/problem.rs
@@ -1,8 +1,10 @@
 use schema::Solver;
 
 mod day1;
+mod day2;
 
 pub const DAYS: &[&dyn Solver] = &[
     &day1::Day01,
+    &day2::Day02,
 ];
 
---
+++ 2023/src/problem/day2.rs
@@ -0,0 +1,89 @@
+use std::{time::{Duration, Instant}, error::Error};
+
+use schema::Solver;
+
+
+pub struct Day02;
+
+fn line_parse(line: &str) -> Result<(u32, u32, u32, u32), Box<dyn Error + Send + Sync>> {
+    let (game, lgame) = line.split_once(":").unwrap();
+
+    let id = game.split_at(5).1.parse::<u32>().unwrap();
+
+    let mut red = 0;
+    let mut blue = 0;
+    let mut green = 0;
+
+    for set in lgame.split(";") {
+        for s in set.split_terminator(",") {
+            let mut c = s.trim().split_whitespace();
+
+            let val = c.next().unwrap().parse::<u32>().unwrap();
+
+            match c.next().unwrap_or_default() {
+                "red" => { red = red.max(val); },
+                "green" => { green = green.max(val); },
+                "blue" => { blue = blue.max(val); },
+                _ => {
+                    eprintln!("Found invalid color at {id}");
+                    return Err(Box::<dyn Error + Send + Sync>::from("Bad input!"));
+                },
+            }
+
+        }
+
+    }
+
+    Ok((id, red, green, blue))
+
+}
+
+impl Solver for Day02 {
+    fn title(&self) -> &'static str {
+        r"Cube Conundrum"
+    }
+
+    fn part_a(&self, input: &str, out: std::sync::Arc<std::sync::Mutex<schema::Answer>>) 
+            -> Result<Duration, Box<(dyn std::error::Error + Send + Sync)>> {
+        let mut total = 0;
+
+        let ts = Instant::now();
+        for line in input.lines() {
+            let (id, red, green, blue) = line_parse(line)?;
+
+            if (red <= 12) && (green <= 13) && (blue <= 14) {
+                total += id;
+            }
+
+        }
+        let d = ts.elapsed();
+
+        let mut output = out.lock().unwrap();
+        *output = total.into();
+
+        Ok(d)
+    }
+
+    fn part_b(&self, input: &str, out: std::sync::Arc<std::sync::Mutex<schema::Answer>>) 
+            -> Result<Duration, Box<(dyn std::error::Error + Send + Sync)>> {
+        let mut total = 0;
+
+        let ts = Instant::now();
+        for line in input.lines() {
+            let (_, red, green, blue) = line_parse(line)?;
+
+            total += red * green * blue;
+
+        }
+        let d = ts.elapsed();
+
+        let mut output = out.lock().unwrap();
+        *output = total.into();
+
+        Ok(d)
+    }
+
+    fn is_empty(&self) -> bool {
+        false
+    }
+}
