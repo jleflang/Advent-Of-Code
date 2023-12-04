8c44b401e60bc7b39eb751e5cbf882c9c18ce87d
James Leflang <59455417+jleflang@users.noreply.github.com>
2023-12-04 14:34:49 -0800

    Day 4 Part A Valid Solution
    Working on Part B

--- 2023/src/problem.rs
+++ 2023/src/problem.rs
@@ -3,10 +3,12 @@ use schema::Solver;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 pub const DAYS: &[&dyn Solver] = &[
     &day1::Day01,
     &day2::Day02,
     &day3::Day03,
+    &day4::Day04,
 ];
 
