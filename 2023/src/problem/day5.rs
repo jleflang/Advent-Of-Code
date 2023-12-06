e322dfac2774c04f2ebb6e04d45034b60e8128a4
James Leflang <59455417+jleflang@users.noreply.github.com>
2023-12-06 12:57:09 -0800

    Learning by doing
    Learned a different way of viewing Part B

--- 2023/src/problem/day5.rs
+++ 2023/src/problem/day5.rs
@@ -1,6 +1,6 @@
 use std::{
     time::{Duration, Instant}, 
-    collections::BTreeMap, 
+    collections::{BTreeMap, VecDeque}, 
 };
 
 use schema::Solver;
@@ -41,29 +41,6 @@ impl Almanac {
         diff_key
     }
 
-    /// INSERT Blade (1998) Quote ABOUT Ice Skating Uphill
-    fn funny_walk(&self, loc: u64) -> u64 {
-
-        let mut diff_key = loc;
-
-        for map in self.maps.iter().rev() {
-
-            // v.1 is the range
-            // v.0 is the destination mapping
-            // key is the source mapping
-            for (&k, &v) in map.iter() {
-                if (diff_key >= v.0) && (diff_key.saturating_sub(v.0) <= v.1) {
-                    diff_key = k + (diff_key - v.0);
-                    break;
-                }
-
-            }
-
-        }
-
-        diff_key
-    }
-
     fn build_almanac(input: &str) -> Self {
     
         let mut sects = input.split("\n\n");
@@ -77,7 +54,7 @@ impl Almanac {
     
         let mut maps = Vec::<Map>::new();
     
-        for s in sects.into_iter().filter(|x| !x.is_empty()) {
+        for s in sects.filter(|x| !x.is_empty()) {
             let lines = s.lines();
     
             let mut map = Map::new();
@@ -137,22 +114,73 @@ impl Solver for Day05 {
         let ts = Instant::now();
         let almanac = Almanac::build_almanac(input);
 
-        let mut loc = almanac.maps.last().unwrap().first_key_value().unwrap().1.0;
-        
-        for location in almanac.maps.last().unwrap().iter().skip(1) {
-            let candidate = almanac.funny_walk(loc);
+        // Co-opted this solution:
+        // https://github.com/Fabi019/aoc2023/blob/main/src/bin/day05.rs#L38
+        let mut seeds: Vec<(u64, u64)> = VecDeque::new().into();
+        seeds.extend(almanac.seeds.chunks_exact(2).map(|s| (s[0], s[0] + s[1])));
 
-            for seed_range in almanac.seeds.chunks_exact(2) {
-                if candidate > seed_range[0] && candidate <= (seed_range[0] + seed_range[1]) {
-                    loc = loc.min(location.1.0);
+        for map in almanac.maps.iter() {
+
+            let map_ranges: Vec<(u64, u64, u64)> = map.iter().map(|(s, (d, r))| (*d, *s, *s+*r)).collect();
+
+            let mut queue: VecDeque<(u64, u64)> = VecDeque::new();
+            queue.extend(seeds.iter());
+
+            let mut new_q: Vec<(u64, u64)> = Vec::new();
+
+            while let Some((start, end)) = queue.pop_front() {
+                let mut found = false;
+
+                for &(target, lower, upper) in &map_ranges {
+                    if start >= lower && start < upper && end < upper {
+                        // Complete range is in bounds
+                        let s = start + target - lower;
+                        let e = end + target - lower;
+                        new_q.push((s, e));
+                        found = true;
+                        break;
+                    } else if start >= lower && start < upper {
+                        // Start is in bounds, end is not
+                        let s = start + target - lower;
+                        let e = upper - 1 + target - lower;
+                        new_q.push((s, e));
+                        queue.push_back((upper, end));
+                        found = true;
+                        break;
+                    } else if start < lower && end >= lower && end < upper {
+                        // End is in bounds, start is not
+                        let s = lower + target - lower;
+                        let e = end + target - lower;
+                        new_q.push((s, e));
+                        queue.push_back((start, lower - 1));
+                        found = true;
+                        break;
+                    } else if start < lower && end >= upper {
+                        // Neither start nor end are in bounds
+                        new_q.push((lower + target - lower, upper - 1 + target - lower));
+                        queue.push_back((upper, end));
+                        queue.push_back((start, lower - 1));
+                        found = true;
+                        break;
+                    }
+                }
+
+                if !found {
+                    // No overlap with any range
+                    new_q.push((start, end));
                 }
             }
+
+            seeds = new_q;
+        
         }
+
+        let location = seeds.iter().min().unwrap().0;
 
         let d = ts.elapsed();
 
         let mut output = out.lock().unwrap();
-        *output = loc.into();
+        *output = location.into();
 
         Ok(d)
     }
