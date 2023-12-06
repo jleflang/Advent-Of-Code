add12f8d8be0ec995f334e79e6fc0f92332c48fa
James Leflang <59455417+jleflang@users.noreply.github.com>
2023-12-06 12:47:10 -0800

    Day 6 Valid Solutions

--- 2023/src/problem/day6.rs
+++ 2023/src/problem/day6.rs
@@ -14,12 +14,70 @@ impl Solver for Day06 {
 
     fn part_a(&self, input: &str, out: std::sync::Arc<std::sync::Mutex<schema::Answer>>) 
             -> Result<std::time::Duration, Box<(dyn std::error::Error + Send + Sync)>> {
-        unimplemented!()
+        
+        let ts = Instant::now();
+        let mut lines = input.lines();
+
+        let times: Vec<u32> = lines.next().unwrap()
+                                        .split_ascii_whitespace()
+                                        .skip(1)
+                                        .map(|s| s.parse().unwrap())
+                                        .collect();
+
+        let dist: Vec<u32> = lines.next().unwrap()
+                                        .split_ascii_whitespace()
+                                        .skip(1)
+                                        .map(|s| s.parse().unwrap())
+                                        .collect();
+
+        let ways: u32 = times.iter().zip(dist.iter())
+                            .map(|(t, d)| {
+                                    let i: u32 = f32::ceil((*t as f32 - f32::sqrt(t.pow(2) as f32 - 4. * *d as f32)) / 2.) as u32;
+                                    t + 1 - 2 * i
+                                })
+                            .product();
+
+        let d = ts.elapsed();
+
+        let mut output = out.lock().unwrap();
+        *output = ways.into();
+
+
+        Ok(d)
+
     }
 
     fn part_b(&self, input: &str, out: std::sync::Arc<std::sync::Mutex<schema::Answer>>) 
             -> Result<std::time::Duration, Box<(dyn std::error::Error + Send + Sync)>> {
-        unimplemented!()
+        let ts = Instant::now();
+        let mut lines = input.lines();
+
+        let time: u64 = lines.next().unwrap()
+                                        .split_ascii_whitespace()
+                                        .skip(1)
+                                        .into_iter()
+                                        .flat_map(|s| s.chars())
+                                        .collect::<String>()
+                                        .parse::<u64>().unwrap();
+
+        let dist: u64 = lines.next().unwrap()
+                                        .split_ascii_whitespace()
+                                        .skip(1)
+                                        .into_iter()
+                                        .flat_map(|s| s.chars())
+                                        .collect::<String>()
+                                        .parse::<u64>().unwrap();
+
+        let i1: u64 = f64::ceil((time as f64 - f64::sqrt(time.pow(2) as f64 - 4. * dist as f64)) / 2.) as u64;
+        let i2: u64 = f64::floor((time as f64 + f64::sqrt(time.pow(2) as f64 - 4. * dist as f64)) / 2.) as u64;
+        let ways = i2 - i1 + 1;
+
+        let d = ts.elapsed();
+
+        let mut output = out.lock().unwrap();
+        *output = ways.into();
+
+        Ok(d)
     }
 
     fn is_empty(&self) -> bool {
